6780b7866329e71b4c2fae08f58a06d70cde9e04
ashishkamathi <32205608+ashishkamathi@users.noreply.github.com>
2021-08-13 14:30:38 +0530

    added label

--- Simple-Assembler/source/main.py
+++ Simple-Assembler/source/main.py
@@ -4,11 +4,10 @@ opp_dic={"add":"A","sub":"A","movi":"B","mov":"C","ld":"D","st":"D","mul":"A","d
 "and":"A","not":"C","cmp":"C","jmp":"E","jlt":"E","jgt":"E","je":"E","hlt":"F"}
 opp_code={"add":"00000","sub":"00001","movi":"00010","mov":"00011","ld":"00100","st":"00101","mul":"00110","div":"00111",
 "rs":"01000","ls":"01001","xor":"01010","or":"01011",
-"and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"10000","10001":"E","je":"10010","hlt":"10011"}
-
+"and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"10000","jgt":"10001","je":"10010","hlt":"10011"}
 reg_add={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
-
 var_add={}
+label_add={}
 var_count=0
 
 def typeA(oc,r1,r2,r3):
@@ -25,13 +24,15 @@ def typeB(oc,r1,im):
     while len(bi)!=8:
         bi="0"+bi
     return oc+reg_add[r1]+bi
-
 def typeC(oc,r1,r2):
+    global reg_add
     return oc+"00000"+reg_add[r1]+reg_add[r2]
-
 def typeD(oc,r1,var):
     global var_add
     return (oc+reg_add[r1]+var_add[var])
+def typeE(oc,adr):
+    global label_add
+    return(oc+"000"+label_add[adr])
 
 def convertb(a):
     li=a.split()
@@ -58,11 +59,11 @@ def convertb(a):
         return(typeD(op_c,li[1],li[2]))
     if type=="C":
         return(typeC(op_c,li[1],li[2]))
+    if type=="E":
+        return(typeE(op_c,li[1]))
  
-
-
 def main():
-    #print(convertb("mov R1 R2"))
+    
     complete_input = sys.stdin.read()
     commands=complete_input.splitlines()
     global finalbin
@@ -70,6 +71,23 @@ def main():
         if "var" in cm:
             global var_count
             var_count=var_count+1             
+    for k in range (len(commands)):
+        temp=commands[k].split()
+        if(":" in temp[0]):
+            global label_add
+            bi=bin(k-var_count).replace("0b", "")
+            while len(bi)!=8:
+                bi="0"+bi
+            label_add[temp[0].replace(":","")]=bi
+            if len(temp)==2:
+                 commands[k]=temp[1]
+            if len(temp)==3:
+                commands[k]=temp[1]+" "+temp[2]
+            if len(temp)==4:
+                commands[k]=temp[1]+" "+temp[2]+" "+temp[3]
+            if len(temp)==5:
+                commands[k]=temp[1]+" "+temp[2]+" "+temp[3]+" "+temp[4]    
+
     for cm in commands:
         temp=cm.split()
         if temp[0]=="var":
@@ -82,8 +100,10 @@ def main():
             var_count=var_count-1
         else:
             finalbin.append(convertb(cm))
-    for k in finalbin:
+ 
+    for k in finalbin:   
         sys.stdout.write(k+"\n")
+    
     
       
 if __name__ == '__main__':
